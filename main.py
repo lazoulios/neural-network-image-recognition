e38a1975679a585896baad1ad58a3340af1a7af6
lazos <112394398+lazoulios@users.noreply.github.com>
2025-11-04 16:49:29 +0200

    steup t model kai tou training.
    to montelo den perase akoma ekpaideusi kathos den exo ton xrono tora

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
 import numpy as np
+import time
 
 print("Φόρτωση δεδομένων CIFAR-10...")
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -17,7 +20,37 @@ y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 input_shape = x_train.shape[1:] 
 
 print("\n--- Επιβεβαίωση ---")
-print(f"Διαστάσεις x_train: {x_train.shape}") # Αναμένεται (50000, 32, 32, 3)
-print(f"Διαστάσεις y_train: {y_train.shape}") # Αναμένεται (50000, 10)
+print(f"Διαστάσεις x_train: {x_train.shape}")
+print(f"Διαστάσεις y_train: {y_train.shape}") 
 print(f"Διαστάσεις Εικόνας Εισόδου (Input Shape): {input_shape}")
 print(f"Αριθμός Κλάσεων: {num_classes}")
+
+model = Sequential([
+        Flatten(input_shape=input_shape),
+        Dense(128, activation='relu'), 
+        Dense(64, activation='relu'),
+        Dense(num_classes, activation='softmax')
+    ])
+
+print("\n\nCompile του Μοντέλου...")
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+EPOCHS = 10
+BATCH_SIZE = 32
+
+print(f"\nΕκπαίδευση μοντέλου για {EPOCHS} εποχές...")
+start_time = time.time()
+
+history = model.fit(x_train, y_train, 
+                    epochs=EPOCHS, 
+                    batch_size=BATCH_SIZE, 
+                    validation_data=(x_test, y_test)) 
+
+end_time = time.time()
+training_time = end_time - start_time   
+
+print(f"\nΟλοκληρώθηκε η εκπαίδευση.")
+print(f"Χρόνος Εκπαίδευσης: {training_time:.2f} δευτερόλεπτα")
